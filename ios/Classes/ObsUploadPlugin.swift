2a2ad85dfb3a40ea3cde506ca1f36125f23aa9f5
lilyjie <lilyjie0823@gmail.com>
2024-09-29 03:36:55 -0700

    submit

--- ios/Classes/ObsUploadPlugin.swift
+++ ios/Classes/ObsUploadPlugin.swift
@@ -2,11 +2,17 @@ import Flutter
 import UIKit
 import OBS
 
-public class ObsUploadPlugin: NSObject, FlutterPlugin {
+public class ObsUploadPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+    var config: OBSServiceConfiguration?
+    private var eventSink: FlutterEventSink?
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "obs_upload_plugin", binaryMessenger: registrar.messenger())
     let instance = ObsUploadPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+      
+      let streamChannel = FlutterEventChannel(name: "obs_upload_plugin/upload", binaryMessenger: registrar.messenger())
+      streamChannel.setStreamHandler(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -19,10 +25,43 @@ public class ObsUploadPlugin: NSObject, FlutterPlugin {
         let sk = arg["sk"]!
         let endPoint = arg["endPoint"]!
         let provider = OBSStaticCredentialProvider.init(accessKey: ak, secretKey: sk)
-        let config = OBSServiceConfiguration.init(url: URL(string: endPoint)!, credentialProvider: provider)
-        let client = OBSClient.init(configuration: config)
+        config = OBSServiceConfiguration.init(url: URL(string: endPoint)!, credentialProvider: provider)
+       
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+    
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events;
+        if let arg = arguments as? [String: Any] {
+            let path = arg["path"] as? String
+            if let config = config {
+                let client = OBSClient.init(configuration: config)
+                // upload setting
+                client!.configuration.maxConcurrentUploadRequestCount = 5;
+                client!.configuration.uploadSessionConfiguration.httpMaximumConnectionsPerHost = 10
+                let request = OBSUploadFileRequest.init(bucketName: "bucketName", objectKey: "objcetname", uploadFilePath: path)
+                request?.partSize = NSNumber(integerLiteral: 5 * 1024 * 1024)
+                request?.enableCheckpoint = true
+                //            request?.checkpointFilePath = "";
+                request?.uploadProgressBlock = {sent, total, totalBytesExpectedToSend in
+                    print("sent = \(sent), total = \(total), totalBytesExpectedToSend = \(totalBytesExpectedToSend)")
+                }
+                let task = client?.uploadFile(request, completionHandler: { response, err in
+                    if (err != nil) {
+                        print("ios upload err: \(err)")
+                    }
+                })
+                // 取消上传
+//                request?.cancel();
+            }
+        }
+        return nil;
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil;
+    }
 }
